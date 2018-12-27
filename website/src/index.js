1039273374eb165e1ac7e2c63f8f600f541a8ea6
Izhaki <roey@gefri.org>
2018-12-27 22:39:16 +0000

    Refactor: Move sunburst related code to own folder.

--- website/src/index.js
+++ website/src/index.js
@@ -1,29 +1,9 @@
 import './index.css';
-import { pipe } from '@codinsky/core'; // eslint-disable-line import/no-extraneous-dependencies
-import parse from '@codinsky/parse-js';
-import curate from '@codinsky/curate';
-import geometrify from '@codinsky/geometrify';
-import render from '@codinsky/render-d3-dom';
-import getVisualisationSize from './getVisualisationSize';
-
-const setSize = (elementId, size) => {
-  const { style } = document.getElementById(elementId);
-  style.width = size;
-  style.height = size;
-};
-
-const size = getVisualisationSize();
-setSize('sun-burst', size);
+import sunburst from './sunburst';
 
 const code = `
 if (a)
   doSomething()
 `;
 
-const root = pipe(
-  parse,
-  curate,
-  geometrify,
-)(code);
-
-render(root, '#sun-burst #outline', size);
+sunburst(code);
---
+++ website/src/sunburst/index.js
@@ -0,0 +1,25 @@
+import { pipe } from '@codinsky/core'; // eslint-disable-line import/no-extraneous-dependencies
+import parse from '@codinsky/parse-js';
+import curate from '@codinsky/curate';
+import geometrify from '@codinsky/geometrify';
+import render from '@codinsky/render-d3-dom';
+import getVisualisationSize from './getVisualisationSize';
+
+const setSize = (elementId, size) => {
+  const { style } = document.getElementById(elementId);
+  style.width = size;
+  style.height = size;
+};
+
+const size = getVisualisationSize();
+setSize('sun-burst', size);
+
+export default code => {
+  const root = pipe(
+    parse,
+    curate,
+    geometrify,
+  )(code);
+
+  render(root, '#sun-burst #outline', size);
+};
