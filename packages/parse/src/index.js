4a89327232007591b6a788507f23c24d0a641905
Izhaki <roey@gefri.org>
2019-01-03 22:58:18 +0000

    Feature: Parse parse typescript, jsx and flow.

--- packages/parse/src/index.js
+++ packages/parse/src/index.js
@@ -1,19 +1,40 @@
 import { parse } from '@babel/parser';
 import normalise from './normalise';
 
-const options = {
-  // parse in strict mode and allow module declarations
-  sourceType: 'module',
+const baseOptions = {
+  sourceType: 'unambiguous',
+};
 
-  plugins: [
-    // enable jsx and flow syntax
-    //    'jsx',
-    'flow',
-    // 'typescript',
-  ],
+const basePlugins = [
+  'asyncGenerators',
+  'bigInt',
+  'classProperties',
+  ['decorators', { decoratorsBeforeExport: false }],
+  'dynamicImport',
+  'exportDefaultFrom',
+  'jsx',
+  'objectRestSpread',
+];
+
+const tsx = {
+  ...baseOptions,
+  plugins: [...basePlugins, 'typescript'],
+};
+
+const jsx = {
+  ...baseOptions,
+  plugins: [...basePlugins, 'flow'],
 };
 
 export default source => {
-  const ast = parse(source, options);
+  let ast;
+
+  for (const options of [tsx, jsx]) {
+    try {
+      ast = parse(source, options);
+    } catch (error) {
+      /* Do nothing */
+    }
+  }
   return normalise(ast);
 };
